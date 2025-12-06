26e13c6c14306f7779fb383afc70fc37ec71fe88
Your Name <you@example.com>
2025-12-06 14:55:53 +0300

    heheh

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 let title = document.getElementById("title");
 let button = document.getElementById("btn");
 let screamer = document.getElementById("screamer");
+let snd = document.getElementById("snd");
 
 button.onclick = function() {
 
 screamer.hidden = false;
+title.hidden = true;
+button.hidden = true;
+
+snd.currentTime = 0;
+snd.play();
+
 
 setTimeout(function() {
 screamer.hidden = true;
+title.hidden = false;
+button.hidden = false;
+
 }, 1000);
 };
